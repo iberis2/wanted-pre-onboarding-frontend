ca75b95fdfafb4de809cfd9d3a6f2fd7515c4d86
iberis2 <snow9402@gmail.com>
2023-08-11 03:46:13 +0900

    Design: 회원가입, 로그인 CSS
    - 현재 있는 페이지에서 반대 페이지로 가는 회원가입(로그인) 하러가기 버튼 추가

--- src/components/SignForm.tsx
+++ src/components/SignForm.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { emailPasswordValidation } from '../lib/validation'
 import { signIn, signUp } from '../apis/auth'
+import styles from './SignForm.module.css'
 
 type SignFormType = {
   type: '로그인' | '회원가입'
@@ -35,27 +36,33 @@ export default function SignForm({ type = '회원가입' }: SignFormType) {
       if (status === 201) {
         navigate('/signin')
       }
-      return
-    }
-
-    if (type === '로그인') {
+    } else if (type === '로그인') {
       const status = await signIn(inputValue)
       if (status === 200) {
         navigate('/todo')
       }
-      return
+    }
+  }
+
+  const moveToOtherPage = () => {
+    if (type === '회원가입') {
+      navigate('/signin')
+    } else if (type === '로그인') {
+      navigate('/signup')
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
         type='email'
         value={inputValue.email}
         onChange={event => handleChange('email', event)}
         onKeyUp={handleKeyUp}
         data-testid='email-input'
+        placeholder='이메일을 입력해주세요'
         required
+        className={styles.input}
       />
       <input
         type='password'
@@ -64,10 +71,20 @@ export default function SignForm({ type = '회원가입' }: SignFormType) {
         onChange={event => handleChange('password', event)}
         onKeyUp={handleKeyUp}
         data-testid='password-input'
+        placeholder='비밀번호를 입력해주세요'
         required
+        className={styles.input}
       />
-      <button type='submit' data-testid='signup-button' disabled={btnDisabled}>
+      <button
+        type='submit'
+        data-testid='signup-button'
+        disabled={btnDisabled}
+        className={btnDisabled ? styles.disabledBtn : styles.button}
+      >
         {type}
+      </button>
+      <button type='button' onClick={moveToOtherPage} className={styles.routToBtn}>
+        {type === '회원가입' ? '로그인 하러 가기' : '회원가입 하러 가기'}
       </button>
     </form>
   )
