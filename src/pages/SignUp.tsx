00ef15c6cc0454e494edf4e517b7632d034169f7
iberis2 <snow9402@gmail.com>
2023-08-10 10:47:40 +0900

    Feat: 회원가입 유효성 검사 완성
    - 이메일 @ 포함, 비밀번호 8자 이상이지 않으면 submit 버튼 disabled 되도록
    - ① 이메일 type email, 비밀번호 minLength 8, required 설정
    -② onKeyUp 할때마다 정규표현식 `.test` 메서드로 검사하여 btnDisabled state 변경

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,54 @@
+import { useState } from 'react'
+
+const emailRegex = /@/
+const passwordRegex = /^.{8,}$/
 export default function SignUp() {
-  return <div>sign up</div>
+  const [inputValue, setInputValue] = useState({
+    email: '',
+    password: '',
+  })
+  const [btnDisabled, setBtnDisabled] = useState(true)
+
+  const handleChange = (type: 'email' | 'password', event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(prev => ({ ...prev, [type]: event.target.value }))
+  }
+
+  const handleKeyUp = () => {
+    if (emailRegex.test(inputValue.email) && passwordRegex.test(inputValue.password)) {
+      setBtnDisabled(false)
+    } else {
+      setBtnDisabled(true)
+    }
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    console.log('IV', inputValue)
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='email'
+        value={inputValue.email}
+        onChange={event => handleChange('email', event)}
+        onKeyUp={handleKeyUp}
+        data-testid='email-input'
+        required
+      />
+      <input
+        type='password'
+        minLength={8}
+        value={inputValue.password}
+        onChange={event => handleChange('password', event)}
+        onKeyUp={handleKeyUp}
+        data-testid='password-input'
+        required
+      />
+      <button type='submit' data-testid='signup-button' disabled={btnDisabled}>
+        회원가입
+      </button>
+    </form>
+  )
 }
